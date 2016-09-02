d636eee91d70eeb45eeb4a6ff5707a82a701d776
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-09-03 00:51:22 +0530

    updated

--- dictbot/urls.py
+++ dictbot/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from main_app.views import dictbot
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dictbot.views.home', name='home'),
